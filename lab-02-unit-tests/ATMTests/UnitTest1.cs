7f250e77e85e4081696284016899fe00db227ff5
Karina Chen <karina6188@gmail.com>
2019-10-16 10:37:58 -0700

    Main method to have an ATM menu

---
+++ lab-02-unit-tests/ATMTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static lab_02_unit_tests.Program;
+
+namespace ATMTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- lab-02-unit-tests/lab-02-unit-tests/Program.cs
+++ lab-02-unit-tests/lab-02-unit-tests/Program.cs
@@ -2,11 +2,80 @@
 
 namespace lab_02_unit_tests
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                bool ATM = true;
+                while (ATM)
+                {
+                    ATM = StartATM();
+                }
+
+                static bool StartATM()
+                {
+                    Console.Clear();
+                    Console.WriteLine("Select your transaction.");
+                    Console.WriteLine("1) View Current Balance");
+                    Console.WriteLine("2) Withdraw Money");
+                    Console.WriteLine("3) Make Deposits");
+                    Console.WriteLine("4) Sign Out");
+                    string service = Console.ReadLine();
+                    if (service == "1")
+                    {
+                        ViewBalance();
+                        return true;
+                    }
+                    else if (service == "2")
+                    {
+                        Withdraw();
+                        return true;
+                    }
+                    else if (service == "3")
+                    {
+                        Deposit();
+                        return true;
+                    }
+                    else if (service == "4")
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your selection is not valid.");
+                        Console.WriteLine("Please select again from the available transactions.");
+                        Console.ReadLine();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There is something wrong with the ATM processing.");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("You have successfully signed out.");
+                Console.WriteLine("Thank you for using the ATM services.");
+            }
+        }
+
+        static void ViewBalance()
+        {
+            Console.WriteLine();
+        }
+
+        static void Withdraw()
+        {
+            Console.WriteLine();
+        }
+
+        static void Deposit()
+        {
+            Console.WriteLine();
         }
     }
 }
